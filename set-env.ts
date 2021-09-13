c7c6f69dd308d2ba67f6fed9581ebca202cac8a8
Pedro Fernandes <pedroice@gmail.com>
2021-09-13 21:33:40 +0200

    chore: add env for local

--- set-env.ts
+++ set-env.ts
@@ -1,16 +1,34 @@
 import ErrnoException = NodeJS.ErrnoException;
 
 const { writeFile } = require('fs');
+const { argv } = require('yargs');
+
+// read environment variables from .env file
+require('dotenv').config();
+
+// read the command line arguments passed with yargs
+const environment = argv.environment;
+const isProduction = environment === 'prod';
+
+if (
+    !process.env.ANGULAR_APP_SUPABASE_URL ||
+    !process.env.ANGULAR_APP_SUPABASE_KEY
+) {
+    console.error('All the required environment variables were not provided!');
+    process.exit(-1);
+}
 
 // Configure Angular `environment.ts` file path
-const targetPath = './src/environments/environment.prod.ts';
+const targetPath = isProduction
+    ? `./src/environments/environment.prod.ts`
+    : `./src/environments/environment.ts`;
 
 // read environment variables from .env file
 require('dotenv').config();
 
 // `environment.ts` file structure
 const envConfigFile = `export const environment = {
-   production: true,
+   production: ${isProduction},
    supabaseUrl: '${process.env.ANGULAR_APP_SUPABASE_URL}',
    supabaseKey: '${process.env.ANGULAR_APP_SUPABASE_KEY}'
   };
